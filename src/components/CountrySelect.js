3c298f352c525cb626ebe3683998da1610ef90b6
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-12-26 10:54:15 +0500

    fetch all countries

--- src/components/CountrySelect.js
+++ src/components/CountrySelect.js
@@ -1,15 +1,30 @@
 import { Container } from "@material-ui/core";
 import InputSelect from "./InputSelect";
 import './styles/country-select.css';
+import { useEffect, useState } from "react";
+import { fetchCountries } from "../api";
 
 const CountrySelect = () => {
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("United States");
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setCountries(await fetchCountries());
+    };
+    
+    fetchAPI();
+  }, []);
   
   return (
     <Container className="country-select" maxWidth="md">
-      <InputSelect value={""} name="inputSelect" handleChange={() => {
-      }} id="input-select"
-      label="Select Country"
-      />
+      <InputSelect isDisabled={!Boolean(countries.length)} value={selectedCountry} name="inputSelect"
+                   handleChange={(e) => setSelectedCountry(e.target.value)}
+                   id="input-select"
+                   label="Select Country"
+      >
+        <option value="">United States</option>
+        {countries.map((country, index) => <option key={String(index)} value={country}>{country}</option>)}
+      </InputSelect>
     </Container>
   
   );
